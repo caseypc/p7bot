4b1ed2ca1cde673d226b900323f96be6ab27f8b0
Robert Whitney <xnite@xnite.me>
2017-11-15 17:17:30 -0600

    improved socket handling, getting closer to ssl support

--- Start.php
+++ Start.php
@@ -3,16 +3,19 @@ class Start
 {
 	private $config;
 	private $socket;
-	private $command_binds = array();
-	private $privmsg_binds = array();
-	private $ping_binds = array();
-	private $nicklist = array();
-	private $running = true;
+	private $command_binds  = array();
+	private $privmsg_binds  = array();
+	private $nick_binds     = array();
+	private $ping_binds     = array();
+	private $nicklist       = array();
+	private $running        = true;
+	private $me;
 	public function __construct()
 	{
 		require_once("Config.php");
 		$this->config = new Config();
-
+		$this->me = new class(){};
+		$this->me->nickname = $this->config->bot['nickname'];
 		try
 		{
 			foreach(glob("plugins/*.php") as $plugin)
@@ -28,21 +31,36 @@ class Start
 		{
 			try
 			{
+				/*
 				$this->socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
 				socket_connect($this->socket, $this->config->network['hostname'], $this->config->network['port']) or die("Could not connect to host: " . $this->config->network['hostname'] . ":" . $this->config->network['port'] . "\n");
+				*/
+				echo "Connecting";
+				while (!is_resource($this->socket))
+				{
+					echo ".";
+					sleep(3);
+					if (isset($this->config->bot['ssl']) && $this->config->bot['ssl'] == true)
+					{
+						$context = stream_context_create();
+						$result = stream_context_set_option($context, 'ssl', $this->config->bot['ssl_cert'], $this->config->bot['ssl_key']);
+						$this->socket = fsockopen("ssl://" . $this->config->network['hostname'], $this->config->network['port'], $errno, $errstr, 15, STREAM_CLIENT_CONNECT, $context);
+					} else
+					{
+						$this->socket = fsockopen("tcp://" . $this->config->network['hostname'], $this->config->network['port'], $errno, $errstr, 15);
+					}
+				}
 				$this->send("NICK " . $this->config->bot['nickname']);
 				$this->send("USER " . $this->config->bot['username'] . " " . $this->config->bot['username'] . " * " . $this->config->bot['realname']);
-				sleep(5);
-				$this->send("JOIN " . implode(",", $this->config->network['channels']));
-			} catch (Exception $e)
+			}catch(Exception $e)
 			{
 				trigger_error($e->getMessage());
 				exit();
 			}
 
-			while ($data = socket_read($this->socket, 4096))
+			while ($data = stream_get_line($this->socket, 1024, "\r\n"))
 			{
-				echo "[IN]\t" . $data;
+				echo "[IN]\t" . $data."\n";
 				$data = str_replace("\r\n", "", $data);
 				// Handle PING
 				if (preg_match("/^PING(.*?)$/i", $data, $m))
@@ -54,20 +72,50 @@ class Start
 					}
 				}
 
+				// Handle nick changes
+				if(preg_match("/^:(.*?)!(.*?)@(.*?) NICK (.*?)$/i", $data, $m))
+				{
+					$eventObject = new class(){};
+					$eventObject->user = new class(){};
+					$eventObject->user->nickname = $m[1];
+					$eventObject->user->username = $m[2];
+					$eventObject->user->hostname = $m[3];
+					$eventObject->oldNick = $m[1];
+					$eventObject->newNick = $m[4];
+					if(strtolower($m[1]) == strtolower($this->me->nickname))
+					{
+						$this->me->nickname = $m[4];
+					}
+
+					foreach ($this->nick_binds as $callback)
+					{
+						$callback($eventObject);
+					}
+				}
+
+				// nick already in use
+				if(preg_match("/^:(.*?) 433 * (.*?) :(.*?)$/i", $data, $m))
+				{
+					$this->me->nickname = $this->me->nickname."_";
+					$this->send("NICK ".$this->me->nickname);
+				}
+				// handle end of /motd
+				if(preg_match("/^:(.*?) 376 (.*?) :(.*?)$/i", $data, $m))
+				{
+					$this->me->server = $m[1];
+					$this->send("JOIN " . implode(",", $this->config->network['channels']));
+				}
+
 				// handle /names
-				if (preg_match("/:(.*?) 353 (.*?) (.*?) (.*?) :(.*?)/i", $data, $m))
+				if (preg_match("/^:(.*?) 353 (.*?) (.*?) (.*?) :(.*?)$/i", $data, $m))
 				{
 					$this->nicklist[$m[4]] = $m[5];
 				}
 				// Handle PRIVMSG
 				if (preg_match("/^:(.*?)!(.*?)@(.*?) PRIVMSG (.*?) :(.*?)$/i", $data, $m))
 				{
-					$eventObject = new class()
-					{
-					};
-					$eventObject->user = new class()
-					{
-					};
+					$eventObject = new class(){};
+					$eventObject->user = new class(){};
 					$eventObject->user->nickname = $m[1];
 					$eventObject->user->username = $m[2];
 					$eventObject->user->hostname = $m[3];
@@ -91,7 +139,7 @@ class Start
 					}
 				}
 			}
-			socket_close($this->socket);
+			fclose($this->socket);
 		}
 	}
 
@@ -99,7 +147,7 @@ class Start
 	{
 		try
 		{
-			socket_write($this->socket, $message."\r\n", strlen($message."\r\n"));
+			fwrite($this->socket, $message."\r\n", strlen($message."\r\n"));
 			echo "[OUT]\t".$message."\n";
 		} catch(Exception $e)
 		{
@@ -112,6 +160,11 @@ class Start
 	public function bindPing($callback)
 	{
 		array_push($this->ping_binds, $callback);
+	}
+
+	public function bindNick($callback)
+	{
+		array_push($this->nick_binds, $callback);
 	}
 
 	public function bindCmd($command, $callback)
