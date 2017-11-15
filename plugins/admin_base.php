0bdf28a008729df96d7e7c27d6f01e1e371680aa
Robert Whitney <xnite@xnite.me>
2017-11-15 17:16:45 -0600

    admin commands

--- plugins/admin_base.php
+++ plugins/admin_base.php
@@ -9,3 +9,30 @@ $this->bindCmd("shutdown",function($event){
 	$this->send("PRIVMSG ".$event->target." :".$event->user->nickname.": Shutting down...");
 	$this->send("QUIT :Shutdown command called by ".$event->user->nickname);
 });
+
+$this->bindCmd("join",function($event){
+	if(!$this->checkAdmin($event->user->mask))
+	{
+		$this->send("PRIVMSG ".$event->target." :".$event->user->nickname.": You do not have permission to do that!");
+		return;
+	}
+	$this->send("JOIN ".$event->arguments[0]);
+});
+
+$this->bindCmd("part",function($event){
+	if(!$this->checkAdmin($event->user->mask))
+	{
+		$this->send("PRIVMSG ".$event->target." :".$event->user->nickname.": You do not have permission to do that!");
+		return;
+	}
+	$this->send("PART ".$event->arguments[0]." /PART command called by ".$event->user->nickname);
+});
+
+$this->bindCmd("nick",function($event){
+	if(!$this->checkAdmin($event->user->mask))
+	{
+		$this->send("PRIVMSG ".$event->target." :".$event->user->nickname.": You do not have permission to do that!");
+		return;
+	}
+	$this->send("NICK ".$event->arguments[0]);
+});
---
+++ plugins/tests.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xnite
+ * Date: 11/15/17
+ * Time: 3:44 PM
+ */
